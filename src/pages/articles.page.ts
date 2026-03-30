f01df953129c31891effba4d932cdbc1c532ad4b
Jacek Majerski <jacekmajerskiqa@gmail.com>
2026-03-30 22:28:52 +0200

    add test articles

--- src/pages/articles.page.ts
+++ src/pages/articles.page.ts
@@ -1,10 +1,13 @@
 import { MainMenuComponent } from '../components/main-menu.component';
-import { BasePage } from './base.page';
+import { BasePage } from '../pages/base.page';
 import { Page } from '@playwright/test';
 
 export class ArticlesPage extends BasePage {
   url = '/articles.html';
   mainMenu = new MainMenuComponent(this.page);
+  articleButtonLogged = this.page.locator('#add-new');
+  articleTitle = this.page.getByTestId('title-input');
+  articleBody = this.page.getByTestId('body-text');
 
   constructor(page: Page) {
     super(page);
